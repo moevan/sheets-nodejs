b17fbbbe2dbdc5abedae9084df73fba50aa9f517
moe <haha>
2020-01-19 21:37:13 +0700

    update

---
+++ auth/spreadsheet_info.js
@@ -0,0 +1,3 @@
+let spreadsheetId = '1wz_ITCm_pehS2loeW2DGeumc8BcyaNt-SD9i8sJqkYE';
+
+module.exports = {spreadsheetId:spreadsheetId};
---
+++ functions/listMajors.js
@@ -0,0 +1,33 @@
+const {google} = require('googleapis');
+const {spreadsheetId} = require('../auth/spreadsheet_info');
+
+
+/**
+ * Prints the names and majors of students in a sample spreadsheet:
+ * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
+ */
+function listMajors(auth) {
+    const sheets = google.sheets({version: 'v4', auth});
+    sheets.spreadsheets.values.get({
+      spreadsheetId: spreadsheetId,
+      range: 'Class Data!A2:E',
+    }, (err, res) => {
+      if (err) return console.log('The API returned an error: ' + err);
+      const rows = res.data.values;
+      if (rows.length) {
+        console.log('Name, Major:');
+        // Print columns A and E, which correspond to indices 0 and 4.
+        rows.map((row) => {
+          console.log(`${row[0]}, ${row[4]}`);
+        });
+      } else {
+        console.log('No data found.');
+      }
+    });
+  }
+
+
+  
+  module.exports = {
+    listMajors: listMajors
+  }
